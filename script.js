1efb268ac7d588e3dccb4a0ad76ef9e87a334885
Telles01 <bernardtelgon@gmail.com>
2020-11-17 16:11:20 -0300

    part II

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+
+const shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 
 
 function startGame () {
-
+    console.log('Started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    shuffledQuestions = questions.sort(() => Math.random( - .5))
+    currentQuestionIndex = 0
+    setNextQuestion()
 }
 
 function setNextQuestion (){
@@ -15,3 +23,13 @@ function setNextQuestion (){
 function selectAnswer (){
 
 }
+
+const questions = [
+    {
+    question: 'What is 2 +2',
+    answer: [
+       { text: '4', correct: true },
+       { text: '22', correct: false} 
+    ]   
+    }
+]
